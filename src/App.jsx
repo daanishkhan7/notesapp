0e41efaff473e474b37cadde2d51bc8b0909d080
daanishkhan7 <dkhan37304@gmail.com>
2025-09-23 17:40:00 -0400

    the notes app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <main style={{ padding: '2rem' }}>
+          <h1>Hello {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-export default App;
 
+export default App;
